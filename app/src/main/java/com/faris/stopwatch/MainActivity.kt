b46d665308874238e0cf1f71f48cfc30f01c9b50
farisrizvanovic <fare.faris2@gmail.com>
2022-05-27 18:20:16 +0200

    Adding viewBinding!

--- app/src/main/java/com/faris/stopwatch/MainActivity.kt
+++ app/src/main/java/com/faris/stopwatch/MainActivity.kt
@@ -6,31 +6,37 @@ import android.os.SystemClock
 import android.widget.Button
 import android.widget.Chronometer
 import android.widget.Toast
+import com.faris.stopwatch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //viewBinding
+    private lateinit var binding: ActivityMainBinding
 
     // Keys for saving the state
     val IS_RUNNING_KEY = "isRunning"
     val OFFSET_KEY = "offset"
     val BASE_KEY = "base"
 
-    lateinit var stopwatch : Chronometer // The stopwatch
+//    lateinit var stopwatch : Chronometer // The stopwatch
     var isRunning = false // Is it running
     var offset : Long = 0 // Offset for the stopwatch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        stopwatch = findViewById(R.id.stopwatch)
+//        stopwatch = findViewById(R.id.stopwatch)
 
         // Restoring the previous state
         if (savedInstanceState !=null ){
             offset = savedInstanceState.getLong(OFFSET_KEY)
             isRunning = savedInstanceState.getBoolean(IS_RUNNING_KEY)
             if (isRunning){
-                stopwatch.base = savedInstanceState.getLong(BASE_KEY)
-                stopwatch.start()
+                binding.stopwatch.base = savedInstanceState.getLong(BASE_KEY)
+                binding.stopwatch.start()
             }
             else {
                 setBaseTime()
@@ -38,29 +44,29 @@ class MainActivity : AppCompatActivity() {
             //toastText("States restored!")
         }
 
-        val startButton = findViewById<Button>(R.id.start_button)
-        val pauseButton = findViewById<Button>(R.id.puse_button)
-        val resetButton = findViewById<Button>(R.id.reset_button)
+//        val startButton = findViewById<Button>(R.id.start_button)
+//        val pauseButton = findViewById<Button>(R.id.puse_button)
+//        val resetButton = findViewById<Button>(R.id.reset_button)
 
-        startButton.setOnClickListener {
+        binding.startButton.setOnClickListener {
             if (!isRunning){
                 setBaseTime()
-                stopwatch.start()
+                binding.stopwatch.start()
                 isRunning = true
             }
            toastText("Stopwatch started!")
         }
 
-        pauseButton.setOnClickListener {
+        binding.puseButton.setOnClickListener {
             if (isRunning){
                 saveOffset()
-                stopwatch.stop()
+                binding.stopwatch.stop()
                 isRunning = false
             }
             toastText("Stopwatch Paused!")
         }
 
-        resetButton.setOnClickListener {
+        binding.resetButton.setOnClickListener {
             offset = 0
             setBaseTime()
         }
@@ -91,7 +97,7 @@ class MainActivity : AppCompatActivity() {
         super.onPause()
         if (isRunning){
             saveOffset()
-            stopwatch.stop()
+            binding.stopwatch.stop()
         }
         toastText("OnPause!")
     }
@@ -101,7 +107,7 @@ class MainActivity : AppCompatActivity() {
         // If the stopwatch was running make it run again when its visible
         if (isRunning){
             setBaseTime()
-            stopwatch.start()
+            binding.stopwatch.start()
             offset = 0
         }
         toastText("OnResume!")
@@ -110,18 +116,18 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putBoolean(IS_RUNNING_KEY, isRunning)
         outState.putLong(OFFSET_KEY, offset)
-        outState.putLong(BASE_KEY, stopwatch.base)
+        outState.putLong(BASE_KEY, binding.stopwatch.base)
         //toastText("States saved!")
         super.onSaveInstanceState(outState)
     }
 
     private fun saveOffset() {
-        offset = SystemClock.elapsedRealtime() -  stopwatch.base
+        offset = SystemClock.elapsedRealtime() -  binding.stopwatch.base
     }
 
     private fun setBaseTime() {
         //Stopwatch.base is the number of milliseconds since the device was booted
-        stopwatch.base = SystemClock.elapsedRealtime() - offset
+        binding.stopwatch.base = SystemClock.elapsedRealtime() - offset
     }
 
     private fun toastText(text: Any){
